8563f44da31aa92b392fe7d62396767e0e0a52c2
Daniel Matthews <dr.dan.matthews@gmail.com>
2015-06-30 12:27:35 +1000

    test image limits

--- Image_Processing/Images_From_ROIs_Advanced.py
+++ Image_Processing/Images_From_ROIs_Advanced.py
@@ -211,7 +211,7 @@ def get_rects_from_rois(conn, imageId):
     parent_image = conn.getObject("Image",imageId)
     sizeX = parent_image.getSizeX()
     sizeY = parent_image.getSizeY()
-    
+    print "image size x, y: ", sizeX,sizeY
     rects = []
     polygons = []
     for roi in result.rois:
@@ -233,7 +233,7 @@ def get_rects_from_rois(conn, imageId):
                     x = sizeX
                 if y > sizeY:
                     y = sizeY
-                    
+                print "roi x, y, w, h: ",x,y,w,h    
                 rects.append( (x,y,w,h,roi_id,name) )
                 break    # Only use the first Rect we find per ROI 
             if name == 'PolygonI':
